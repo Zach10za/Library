339495c68c1f4be0dbdc0499a868a81d719317dc
Zach Loza <zach.loza@gmail.com>
2018-11-29 01:10:12 -0800

    removed redux devtools

--- src/index.js
+++ src/index.js
@@ -1,19 +1,15 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
+import { createStore, applyMiddleware, } from 'redux';
 import thunk from "redux-thunk";
 import rootReducer from './reducers'
 import App from './components/App'
 import './scss';
 
-
 const store = createStore(
   rootReducer,
-  compose(
-    applyMiddleware(thunk),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-  )
+  applyMiddleware(thunk)
 );
 
 render(
